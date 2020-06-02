46a4e55ea0712010f1f9b84aaccccb78ec124d64
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-06-02 13:12:09 -0400

    3

--- client/components/AddCourse.js
+++ client/components/AddCourse.js
@@ -1,44 +1,108 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 import socketIOClient from 'socket.io-client'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 var socket = socketIOClient('http://127.0.0.1:8080')
+var qs = require('qs')
 
-export default class AddCourse extends Component {
+class AddCourse extends Component {
   constructor() {
     super()
     this.state = {
-      message: ''
+      roomName: 'JavaScript',
+      userName: ''
     }
+    this.a = this.a.bind(this)
   }
-  componentDidMount() {
-    // let a = socket.on('time', function(data) {
-    //   console.log(data, typeof data)
-    //   this.setState({response: data})
-    // })
-    // console.log('aaaaaaa', a.io.connecting[0].connected)
-    // a = a.io.connecting[0].connected
-    // a = {response: a}
-    // this.setState(a)
-  }
+
   a() {
-    socket.on('chat message', function(msg) {
+    socket.on('message', function(msg) {
       console.log(msg)
+      let li = document.createElement('li')
+      li.innerHTML = `${msg}`
       let mes = document.getElementById('message')
-      mes.innerHTML = `${mes.innerHTML}<li>${msg}</li>`
-      console.log(mes)
+      //mes.innerHTML = `${mes.innerHTML}<li>${msg}</li>`
+      mes.appendChild(li)
+      this.setState({message: ''})
     })
   }
   render() {
+    const {user} = this.props
+    console.log('useruser', user)
+    const {username, room} = qs.parse(location.search, {
+      ignoreQueryPrefix: true
+    })
     var el
     return (
       <div>
+        <div>
+          <header className="join-header">
+            <h1>ChatCord</h1>
+          </header>
+
+          <form
+            onSubmit={e => {
+              e.preventDefault()
+            }}
+          >
+            <div>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter username..."
+                required
+                onChange={e => {
+                  this.setState({userName: e.target.value})
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="room">Room</label>
+              <select
+                name="room"
+                id="room"
+                onChange={e => {
+                  this.setState({roomName: e.target.value})
+                  console.log(this.state.roomName)
+                }}
+              >
+                <option value="JavaScript">JavaScript</option>
+                <option value="Python">Python</option>
+                <option value="PHP">PHP</option>
+                <option value="C#">C#</option>
+                <option value="Ruby">Ruby</option>
+                <option value="Java">Java</option>
+              </select>
+            </div>
+            <Link
+              to={`/chat?userName=${this.state.userName}&room=${
+                this.state.roomName
+              }`}
+            >
+              Join Chat
+              {/* <button
+              // onClick={() => {
+              //   this.props.history.push(
+              //     `/chat?userName=${this.state.userName}&room=${this.state.roomName}`
+              //   )
+              // }}
+              >
+                Join Chat
+              </button> */}
+            </Link>
+          </form>
+        </div>
         <ul id="message" />
-        <form
+        {/* <form
           id="socket"
           onSubmit={e => {
             e.preventDefault()
             socket.emit('chat message', this.state.message)
-            this.a()
+
+            // this.a()
           }}
         >
           <input
@@ -47,10 +111,22 @@ export default class AddCourse extends Component {
               this.setState({message: e.target.value})
             }}
           />
-          <button>Send</button>
+          <button
+            onClick={e => {
+              this.a()
+            }}
+          >
+            Send
+          </button>
           <button>hello</button>
-        </form>
+        </form> */}
       </div>
     )
   }
 }
+const mapState = ({user}) => {
+  return {
+    user
+  }
+}
+export default connect(mapState)(AddCourse)
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,16 +1,45 @@
+const formatMessage = require('../utils/helpers')
+const {userJoin, getCurrentUser, usersForRoom} = require('../utils/user')
+
 module.exports = io => {
+  const name = 'alex'
   io.on('connection', socket => {
+    socket.on('joinRoom', ({userName, room}) => {
+      const user = userJoin(socket.id, userName, room)
+      console.log(user)
+      socket.join(user.room)
+
+      socket.broadcast
+        .to(user.room)
+        .emit('message', formatMessage(userName, 'welcome'))
+    })
+    //socket.emit('message', formatMessage(userName, 'welcome'))
+    socket.on('chat-message', msg => {
+      const user = getCurrentUser(socket.id)
+      console.log(user)
+
+      io
+        .to(user.room)
+        .emit('chat-message', formatMessage(msg.userName, msg.message))
+    })
+    socket.on('getUsers', msg => {
+      const user = getCurrentUser(socket.id)
+      let roomUsers = usersForRoom(msg.room)
+      io.to(user.room).emit('getUsers', roomUsers)
+      console.log('room users', roomUsers)
+    })
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('disconnect', () => {
+      io.emit('message', formatMessage(name, 'user disconected'))
       console.log(`Connection ${socket.id} has left the building`)
     })
   })
-  io.on('connection', socket => {
-    socket.on('chat message', msg => {
-      console.log('message: ' + msg)
-      io.emit('chat message', msg)
-    })
-  })
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+  // io.on('connection', socket => {
+  //   socket.on('chat message', msg => {
+  //     console.log('message: ' + msg)
+  //     io.emit('chat message', msg)
+  //   })
+  // })
+  //setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
 }
